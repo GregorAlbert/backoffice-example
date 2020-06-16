70d4ce62b24898f17355244c3e412834d9604f8f
Gregor Albert <gregoralbert@gmail.com>
2020-06-16 18:51:59 +0200

    Route unit tests

--- src/app/core/menu/menu.component.spec.ts
+++ src/app/core/menu/menu.component.spec.ts
@@ -1,25 +1,46 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MenuComponent } from './menu.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 
 describe('MenuComponent', () => {
   let component: MenuComponent;
   let fixture: ComponentFixture<MenuComponent>;
+  let router: Router;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
+      declarations: [
+        MenuComponent
+      ],
+      imports: [
+        RouterTestingModule.withRoutes([])
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(MenuComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should route to Dashboard', () => {
+    const navigateSpy = spyOn(router, 'navigate');
+    component.navigate('dashboard');
+    expect(navigateSpy).toHaveBeenCalledWith(['/dashboard']);
+  });
+
+  it('should route to Users', () => {
+    const navigateSpy = spyOn(router, 'navigate');
+    component.navigate('users');
+    expect(navigateSpy).toHaveBeenCalledWith(['/users']);
   });
 });
